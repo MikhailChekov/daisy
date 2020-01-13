f8c4f3cf8074ef1c59e8d60856f5040bc4826f46
Mikhail Chekov <chekoff@list.ru>
2020-01-13 15:17:46 +0300

    Added animate for links, changed main.scss, separated for many files

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,33 @@
 (function () {
-    let mixer = mixitup('.container-filter');
-    let elem = $('[data-filter="all"]');
-    let elems = $('.single-cat');
+    $(document).ready(function () {
+        /*------------animate for links ------------*/
+        $('a[href^="#"]').click(function () {
+            let target = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(target).offset().top
+            }, 500);
+            return false;
+        });
 
-    let change1 = $('#ex1');
-    let change2 = $('#ex6');
+        /*--------------gallery fix ----------------*/
+        let mixer = mixitup('.container-filter');
+        let elem = $('[data-filter="all"]');
+        let elems = $('.single-cat');
 
-    elem.on('click', function() {
-        change1.css('grid-column', '');
-        change2.css('grid-column', '');
-        change1.css('grid-column', 1/3);
-        change2.css('grid-column', 3/5);
+        let change1 = $('#ex1');
+        let change2 = $('#ex6');
+
+        elem.on('click', function() {
+            change1.css('grid-column', '');
+            change2.css('grid-column', '');
+            change1.css('grid-column', 1/3);
+            change2.css('grid-column', 3/5);
+        });
+
+
+        elems.on('click', function() {
+            change1.css('grid-column', 'auto');
+            change2.css('grid-column', 'auto');
+        });
     });
-
-    
-    elems.on('click', function() {
-        change1.css('grid-column', 'auto');
-        change2.css('grid-column', 'auto');
-    });
-
-
 })();
